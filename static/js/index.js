a324475bf8e112cc01fbd4ca7dd2ed5642dc3e51
hyttttt <bianca20011228@gmail.com>
2023-05-06 21:38:44 +0800

    feat: add loading page & connect api

--- static/js/index.js
+++ static/js/index.js
@@ -1,13 +1,13 @@
 window.onload = function () {
   // get hash list
-  var list;
   fetch("/api/binary")
     .then((response) => {
       return response.json();
     })
     .then((response) => {
-      list = response;
+      console.log("hash list:");
       console.log(response);
+      loadList(response);
     })
     .catch((error) => console.error(error));
 };
@@ -19,6 +19,8 @@ function uploadFile() {
   var formData = new FormData();
   formData.append("file", file);
 
+  loadingPage();
+
   fetch("/api/binary", {
     method: "POST",
     body: formData,
@@ -27,22 +29,47 @@ function uploadFile() {
       return response.json();
     })
     .then((response) => {
-      // redirect to cfg page and bring data along
-      //window.location.href = "binary/" + response.hash;
+      analyzingDone(response.hash);
+      console.log("upload hash:");
       console.log(response);
     })
     .catch((error) => console.error(error));
+}
+
+function loadingPage() {
+  var oldDiv = document.getElementById("contentArea");
+
+  // create new div
+  var newDiv = document.createElement("div");
+  newDiv.className = "spinner-border text-purple mySpinner";
+  newDiv.role = "status";
+
+  // find parent div
+  var parent = oldDiv.parentNode;
+  parent.replaceChild(newDiv, oldDiv);
+}
+
+function analyzingDone(hash) {
+  // check if analysis done every 10 sec
+  setInterval(function () {
+    fetch(`/api/binary/${hash}`)
+      .then((response) => response.json())
+      .then((response) => {
+        if (response != null) window.location.href = "binary/" + hash;
+      })
+      .catch((error) => console.error(error));
+  }, 10000);
 }
 
 // load the binaries we have and make it a list
 function loadList(list) {
   var myList = document.getElementById("myList");
 
-  for (i = 0; i < list.hash_list.length; i++) {
+  for (i = 0; i < list.length; i++) {
     const newLi = document.createElement("a");
-    newLi.innerHTML = list.hash_list[i];
+    newLi.innerHTML = list[i];
     newLi.className = "list-group-item";
-    newLi.href = "binary/" + list.hash_list[i];
+    newLi.href = "binary/" + list[i];
 
     newLi.onmouseover = function () {
       this.className = "list-group-item active";
